823b0c666844ecd77f02b76dc8c9915a8cc9019f
brunexmg <brunorafael.o@hotmail.com>
2016-10-06 20:20:46 -0300

    V_1.0.0

---
+++ src/main/java/com/oficina/saude/SaudepublicaApplication.java
@@ -0,0 +1,12 @@
+package com.oficina.saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudepublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudepublicaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oficina/saude/SaudepublicaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oficina.saude;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaudepublicaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
