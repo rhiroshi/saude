efab45d288514e3e854eb01d814504cdb5e97d26
brunexmg <brunorafael.o@hotmail.com>
2016-10-13 22:09:57 -0300

    V_2.0.0

--- src/main/java/com/oficina/saude/SaudepublicaApplication.java
+++ src/main/java/com/oficina/saude/SaudepublicaApplication.java
@@ -1,7 +1,12 @@
 package com.oficina.saude;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class SaudepublicaApplication {
@@ -9,4 +14,10 @@ public class SaudepublicaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaudepublicaApplication.class, args);
 	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
 }
--- src/main/java/com/oficina/saude/controller/PacienteController.java
+++ src/main/java/com/oficina/saude/controller/PacienteController.java
@@ -1,17 +1,34 @@
 package com.oficina.saude.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("pacientes")
+@RequestMapping("/pacientes")
 public class PacienteController {
 
+	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("/paciente/CadastroPasciente");
+		ModelAndView mv = new ModelAndView("/paciente/CadastroPaciente");
 		
 		return mv;
+	}
+	
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
+	public ModelAndView cadastrar(/*@Valid Paciente paciente,*/ BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return novo();
+		}
+		//cadastroCursoService.salvar(curso);
+		attributes.addFlashAttribute("mensagem","Paciente cadastrado com sucesso!");
+		
+		return new ModelAndView("redirect:/pacientes/novo");
 	}
 	
 }
