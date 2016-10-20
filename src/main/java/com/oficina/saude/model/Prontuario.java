fbbcf1845962067ee727b067b3177f1daccd7cb0
rhiroshi <rhiroshi14@hotmail.com>
2016-10-20 14:57:41 -0200

    prontuario

--- src/main/java/com/oficina/saude/model/Prontuario.java
+++ src/main/java/com/oficina/saude/model/Prontuario.java
@@ -1,11 +1,24 @@
 package com.oficina.saude.model;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 @Entity
 public class Prontuario {
 
 	@Id
-	private int id;
-	pressao data prescricao altura peso
-	private String 
+	private Integer id;
+	@NotNull(message="Pressão é obrigatório")
+	private String pressao;
+	@NotNull(message="Data é obrigatório")
+	private Date data;
+	private String prescricao;
+	@NotNull(message="Altura é obrigatório")
+	private float altura;
+	@NotNull(message="Peso é obrigatório")
+	private float peso;
 	
 }
