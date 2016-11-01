12808db35f1ea161aa2baf119c016038c0ca7b65
rhiroshi <rhiroshi14@hotmail.com>
2016-11-01 19:34:31 -0200

    Adicionado mascara em javascript

---
+++ src/main/resources/static/javascripts/mascara.js
@@ -0,0 +1,12 @@
+$('document').ready(function($){
+	var cpf = $('#cpf');
+	var rg = $('#rg');
+	var dataNascimento = $('#dataNascimento');
+	var telefoneResidencial = $('#telefoneResidencial');
+	var telefoneCelular = $('#telefoneCelular');
+	telefoneResidencial.mask('(00)0000-0000');
+	telefoneCelular.mask('(00)0000-0000');
+	dataNascimento.mask('00/00/0000');
+	rg.mask('0.000.000-0');
+	cpf.mask('000.000.000-00');
+});
