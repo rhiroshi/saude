133e3d0ee1d923d06d0bb6dce8da5305c4d5170f
brunexmg <brunorafael.o@hotmail.com>
2016-11-03 14:49:40 -0200

    V_2.1.1

--- src/main/java/com/oficina/saude/controller/ProntuarioController.java
+++ src/main/java/com/oficina/saude/controller/ProntuarioController.java
@@ -1,5 +1,18 @@
 package com.oficina.saude.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/prontuarios")
 public class ProntuarioController {
 
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("prontuario/CadastroProntuario");
+		
+		return mv;
+	}
+	
 }
--- src/main/java/com/oficina/saude/model/Prontuario.java
+++ src/main/java/com/oficina/saude/model/Prontuario.java
@@ -5,9 +5,12 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -22,6 +25,8 @@ public class Prontuario implements Serializable {
 	private Long id;
 	
 	@NotNull(message = "Paciente é obrigatório")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "codigo_paciente")
 	private Paciente paciente;
 	
 	private String pressao;
