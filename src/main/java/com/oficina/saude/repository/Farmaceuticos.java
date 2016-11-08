40b140a6766857b2b63ec463728b977f121509a1
rhiroshi <rhiroshi14@hotmail.com>
2016-11-08 19:24:05 -0200

    Integer -> Long

--- src/main/java/com/oficina/saude/repository/Farmaceuticos.java
+++ src/main/java/com/oficina/saude/repository/Farmaceuticos.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oficina.saude.model.Farmaceutico;
 
-public interface Farmaceuticos extends JpaRepository<Farmaceutico, Integer>{
+public interface Farmaceuticos extends JpaRepository<Farmaceutico, Long>{
 
 }
--- src/main/java/com/oficina/saude/repository/Medicos.java
+++ src/main/java/com/oficina/saude/repository/Medicos.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oficina.saude.model.Medico;
 
-public interface Medicos extends JpaRepository<Medico,Integer>{
+public interface Medicos extends JpaRepository<Medico,Long>{
 
 }
