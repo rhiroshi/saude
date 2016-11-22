f5d71039fc10d9a7ad58530e29186ba25628a2f7
brunexmg <brunorafael.o@hotmail.com>
2016-11-21 22:22:38 -0200

    v_7.0.0-REALESE

---
+++ src/main/java/com/oficina/saude/config/init/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.oficina.saude.config.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	
+	
+}
