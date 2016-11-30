84517c3988a20fb3f43030721b81def8b0ec7379
brunexmg <brunorafael.o@hotmail.com>
2016-11-30 16:08:40 -0200

    7.1.2 - Apenas atualização simples

--- src/main/java/com/oficina/saude/config/SecurityConfig.java
+++ src/main/java/com/oficina/saude/config/SecurityConfig.java
@@ -23,8 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		/*auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("ADMIN");*/
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("ADMIN");
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
