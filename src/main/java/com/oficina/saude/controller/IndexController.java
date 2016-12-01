c54001a44f1c3f4de6d4c5ee864bffe22ab92b17
brunexmg <brunorafael.o@hotmail.com>
2016-12-01 15:23:13 -0200

    v_7.3.6

--- src/main/java/com/oficina/saude/controller/IndexController.java
+++ src/main/java/com/oficina/saude/controller/IndexController.java
@@ -3,10 +3,13 @@ package com.oficina.saude.controller;
 import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oficina.saude.model.Roles;
 import com.oficina.saude.repository.Pacientes;
 import com.oficina.saude.repository.Prontuarios;
 
@@ -22,11 +25,17 @@ public class IndexController {
 	
 	@RequestMapping
 	public ModelAndView novo() {
-		java.util.Date udata = new java.util.Date();
-		Date data = new Date(udata.getTime());
-		ModelAndView mv = new ModelAndView("/index/Index");
-		mv.addObject("pacientesCadastrados", pacientes.count());
-		mv.addObject("consultasHoje", consultas.countByData(data));
+		SecurityContext context = SecurityContextHolder.getContext();
+		ModelAndView mv;
+		if (context.getAuthentication().getAuthorities().equals(Roles.PACIENTE)) {
+			mv = new ModelAndView("alterar link");
+		} else {
+			java.util.Date udata = new java.util.Date();
+			Date data = new Date(udata.getTime());
+			mv = new ModelAndView("/index/Index");
+			mv.addObject("pacientesCadastrados", pacientes.count());
+			mv.addObject("consultasHoje", consultas.countByData(data));
+		}
 		return mv;
 	}
 	
