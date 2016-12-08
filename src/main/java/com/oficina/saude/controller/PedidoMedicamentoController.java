2ef8cca0ebc856441b4b14ab756404c736ceb7ed
rhiroshi <rhiroshi14@hotmail.com>
2016-12-08 19:31:00 -0200

    ++

--- src/main/java/com/oficina/saude/controller/PedidoMedicamentoController.java
+++ src/main/java/com/oficina/saude/controller/PedidoMedicamentoController.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.oficina.saude.model.Consulta;
 import com.oficina.saude.model.PedidoMedicamento;
-import com.oficina.saude.model.Receita;
+import com.oficina.saude.repository.Consultas;
 import com.oficina.saude.repository.Receitas;
 import com.oficina.saude.service.CadastroPedidosMedicamentosService;
 import com.oficina.saude.service.CadastroReceitaService;
@@ -28,9 +28,18 @@ public class PedidoMedicamentoController {
 	@Autowired
 	private CadastroReceitaService receitaService;
 	
+	@Autowired
+	private Consultas consultas;
+	
 	@RequestMapping(value="produtos/novoPedido", method=RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody ResponseEntity<?> novoPedido(@RequestBody PedidoMedicamento pedidoMedicamento){
 		if(!receitas.exists(pedidoMedicamento.getReceita().getCodigo())){
+			Consulta cons1 = consultas.findOne(new Long(1));
+			Consulta cons = new Consulta();
+			cons.setProntuario(cons1.getProntuario());
+			cons.setMedico(cons1.getMedico());
+			cons.setId(pedidoMedicamento.getReceita().getConsulta().getId());
+			consultas.save(cons);
 			receitaService.salvar(pedidoMedicamento.getReceita());
 		}
 		PedidoMedicamento ret = pedidosMedicamentosService.salvar(pedidoMedicamento);
